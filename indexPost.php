916a520677bc96496bfd1fefb015c08891c71cb0
AlexanderPi <AlexanderPi@users.noreply.github.com>
2018-11-02 18:10:38 +0200

    Add files via upload

---
+++ indexPost.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>
+      <form action="GET.php" method="get">
+        <b> Name :</b> <input type="text" name="stu_name">
+        <b> Age :</b> <input type="text" name="stu_age">
+        <b> Weight :</b> <input type="text" name="stu_weight">
+        <br>
+        <input type="submit">
+
+      </form>
+    </h1>
+  </body>
+</html>
